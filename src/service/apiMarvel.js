ff9ab50c02beae02321bddd68a1c0ecbc650a091
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2023-10-17 21:28:29 -0600

    mofificacion dell fetch

--- src/service/apiMarvel.js
+++ src/service/apiMarvel.js
@@ -30,12 +30,10 @@ export const mostraPorId=(id,setdata)=>{
 
 
 export const comicId=(id,setdata)=>{
-  const urlc=`http://gateway.marvel.com/v1/public/characters/${id}/comics?apikey=2b47561ad110f7c9f98eb60beb13685b&hash=134f9effb44638621e33997188d79e93&ts=1`
   try {
-      axios.get(urlc)
-     .then(res=>{
-      setdata(res.data.data.results)
-     })
+     fetch(`http://gateway.marvel.com/v1/public/characters/${id}/comics?apikey=2b47561ad110f7c9f98eb60beb13685b&hash=134f9effb44638621e33997188d79e93&ts=1`)
+     .then(res=>res.json())
+     .then(dat=>setdata(dat.data.results)) 
      .catch(error=>console.log(error))
     } catch (error) {
       console.log(error)
@@ -43,13 +41,11 @@ export const comicId=(id,setdata)=>{
 }
 
 export const seriesId=(id,setdata)=>{
-  const urlc=`http://gateway.marvel.com/v1/public/characters/${id}/series?apikey=2b47561ad110f7c9f98eb60beb13685b&hash=134f9effb44638621e33997188d79e93&ts=1`
   try {
-      axios.get(urlc)
-     .then(res=>{
-      setdata(res.data.data.results)
-     })
-     .catch(error=>console.log(error))
+    fetch(`http://gateway.marvel.com/v1/public/characters/${id}/series?apikey=2b47561ad110f7c9f98eb60beb13685b&hash=134f9effb44638621e33997188d79e93&ts=1`)
+    .then(res=>res.json())
+    .then(dat=>setdata(dat.data.results))
+    .catch(error=>console.log(error))
     } catch (error) {
       console.log(error)
     }
